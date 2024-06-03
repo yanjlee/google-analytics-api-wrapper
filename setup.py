83400bbeefdecdf4f87000473a84e0dc8f0e2b8b
shadow <yanjlee@163.com>
2024-06-03 17:50:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author_email='tikazyq@gmail.com',
     description='The Goolge Analytics wrapper is a convenient tool to extract data from GA via API. It is especially '
                 'useful when the user has many GA profiles / web properties.',
-    keywords=['google-analytics', 'ga', 'pandas', 'dataframe', 'api']
+    keywords=['google-analytics', 'ga', 'pandas', 'dataframe', 'api','wrapper']
 )
